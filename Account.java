eaf817d5af67fe0cacd99d55afcabb9f3c0cf62f
hamdaislam1234 <46087454+hamdaislam1234@users.noreply.github.com>
2018-12-23 23:25:12 +0200

    Update Account.java

--- Account.java
+++ Account.java
@@ -1,1 +1,31 @@
+import java.util.Properties;
+import java.util.*;  
+import javax.mail.*;  
+import javax.mail.internet.*;  
+import javax.activation.*;  
 
+public class Account {
+    String Username;
+    String password;
+    String Email;
+    Feature f;
+    
+    Account(){
+    	//
+    }
+    Account(String Username,String password,String Email){
+        this.Username = Username;
+        this.password = password;
+        this.Email = Email;
+        f = new Feature(this);
+    }
+    Account(String Username,String password){
+        this.Username = Username;
+        this.password = password;
+        this.Email = Email;
+        f = new Feature(this);
+    }
+    
+
+
+}
